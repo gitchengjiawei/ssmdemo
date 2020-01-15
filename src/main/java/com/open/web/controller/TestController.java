2aa2899fe8f69f609b818c3e71af52fc8e349a73
程佳伟 <chengjiawei0330@163.com>
2020-01-15 15:52:48 +0800

    添加 mybatis分页插件

--- src/main/java/com/open/web/controller/TestController.java
+++ src/main/java/com/open/web/controller/TestController.java
@@ -2,11 +2,10 @@ package com.open.web.controller;
 
 import com.open.web.config.ConfigMap;
 import com.open.web.constant.ResponseCode;
-import org.aspectj.apache.bcel.classfile.Constant;
+import com.open.web.utils.QueryData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -19,11 +18,20 @@ import org.springframework.web.servlet.ModelAndView;
 public class TestController extends BaseController {
 
     @GetMapping("testConfig")
-    public ModelAndView testConfig(){
+    public ModelAndView testConfig() {
+        QueryData qd = this.getQueryData();
         System.out.println("request /test/testConfig map:" + ConfigMap.getMap());
         System.out.println("request /test/testConfig map:" + ConfigMap.getMap());
         System.out.println("request /test/testConfig map:" + ConfigMap.getMap());
         System.out.println("request /test/testConfig map:" + ConfigMap.getMap());
-        return getModelAndView("login", ResponseCode.OK,"success");
+        return getModelAndView("login", ResponseCode.OK, "success").addAllObjects(qd);
+    }
+
+    public ModelAndView testPaging(){
+        QueryData qd = this.getQueryData();
+        System.out.println("params is : " + qd);
+
+        ModelAndView result = getModelAndView("list",ResponseCode.OK,"success");
+        return result;
     }
 }
